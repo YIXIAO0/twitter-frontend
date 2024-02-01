eb242d07eb9c26feb39dc732067fb5aae906a403
YIXIAO0 <yxiao84@wisc.edu>
2024-01-31 20:23:59 -0800

    feat: 🎸 add login demo by antd-mobile
    add login demo by antd-mobile

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,26 +1,37 @@
 import { useState } from 'react';
+import { Button, Input, Form, Dialog, PasscodeInput, NumberKeyboard} from 'antd-mobile';
 import './index.css';
-
+const initialValues = {
+  username: 'admin',
+  password: '12345'
+}
 
 const Login = () => {
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const clickHandler = () => {
-    alert('Log in successfully!' + name + ',' + password) ;
-  }
-
-  const onChangeNameHandler = (e) => {
-    setName(e.target.value);
-  }
-
-  const onChangePasswordHandler = (e) => {
-    setPassword(e.target.value);
+  const [form] = Form.useForm()
+  const onSubmit = () => {
+    const values = form.getFieldsValue()
+    Dialog.alert({
+      content: <pre>{JSON.stringify(values, null, 2)}</pre>,
+    })
   }
   return (
     <div className="login">
-        <div>Username: <input onChange={onChangeNameHandler} /> </div>
-        <div>Password: <input type="password" onChange={onChangePasswordHandler} /> </div>
-        <div><button onClick={clickHandler}>Log in</button></div>
+        <Form 
+          form={form}
+          layout='horizontal' model='card' initialValues={initialValues}
+          footer={
+            <Button block color='primary' onClick={onSubmit} size='large'>
+              Log in
+            </Button>
+          }
+        >
+          <Form.Item label='Username' name='username'>
+            <Input placeholder='Please enter username' clearable />
+          </Form.Item>
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='Please enter password' clearabe type='password'/>
+          </Form.Item>
+        </Form>
     </div>
   );
 }
